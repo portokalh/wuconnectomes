6bd7b8eaa03e5311f97485d1b22e98344e4c6894
alex <alex@santorini.dhe.duke.edu>
2021-05-20 16:42:20 -0400

    Deleting a bunch of old defunc files (may bring some back later)

--- getsmallertrk.py
+++
@@ -1,9 +0,0 @@
-
-from tract_manager import reducetractnumber
-
-#oldtrkfile='/Volumes/dusom_dibs_ad_decode/all_staff/munin3badea/mouse/C57_JS/TRK_RAS_40subj/N57449_wholebrain_all_stepsize_2.trk'
-#newfile = '/Volumes/dusom_dibs_ad_decode/all_staff/munin3badea/mouse/C57_JS/TRK_RAS_40subj/N57449_wholebrain_little_stepsize_2.trk'
-oldtrkfile = '/Volumes/dusom_dibs_ad_decode/all_staff/VBM_whiston_QA/H29056_stepsize_2_ratio_100_wholebrain_pruned.trk'
-newfile = '/Volumes/dusom_dibs_ad_decode/all_staff/VBM_whiston_QA/H29056_stepsize_2_ratio_1000_wholebrain_pruned.trk'
-
-reducetractnumber(oldtrkfile, newfile, False, 10, False)
--- launch_CSA_qsubs.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-begin=3
-end=7
-for i in {30..40}
-do
-    qsub -m bea -e ~/wuconnectomes/errorlog.e -o ~/wuconnectomes/whiston_inclusive_$i.o -M jacques.stout@duke.edu -N coninclusive$i -l h_vmem=200G -pe smp 1 -b y python3 ~/wuconnectomes/CSA_createtrkconnect_whitson.py -b $i -e $i
-done
