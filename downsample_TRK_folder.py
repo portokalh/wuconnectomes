965d4a2fe1c038f1169d148ec5c37e6f937b9bd9
alex <alex@santorini.dhe.duke.edu>
2022-02-14 19:34:09 -0500

    Adding random shuffle to downsample and fix for easier multitasking

--- downsample_TRK_folder.py
+++ downsample_TRK_folder.py
@@ -1,6 +1,7 @@
 import os
 from tract_handler import reducetractnumber
 from file_tools import mkcdir
+import random
 
 trk_folder = '/mnt/paros_MRI/jacques/AMD/TRK_MDT_fixed'
 new_trk_folder = '/mnt/paros_MRI/jacques/AMD/TRK_MDT_fixed_100'
@@ -13,19 +14,21 @@ new_trk_folder = '/Users/alex/jacques/AD_decode_comparison_100_temp/'
 trk_folder = '/Volumes/Data/Badea/Lab/human/AD_Decode/Analysis/TRK_MPCA'
 new_trk_folder = '/Volumes/Data/Badea/Lab/human/AD_Decode/Analysis/TRK_MPCA_100'
 
-trk_folder = '/Volumes/Data/Badea/Lab/human/AD_Decode/Analysis/TRK_MPCA_MDT_fixed'
+trk_folder = '/Volumes/Data/Badea/Lab/human/AD_Decode/Analysis/TRK_MPCA_MDT_fixed_refixed'
 new_trk_folder = '/Volumes/Data/Badea/Lab/human/AD_Decode/Analysis/TRK_MPCA_MDT_fixed_100'
 
 mkcdir(new_trk_folder)
 ratio = 100
 filelist = os.listdir(trk_folder)
 filelist = sorted(filelist)
-filelist.reverse()
+random.shuffle(filelist)
+#filelist.reverse()
 
 for filepath in filelist:
     filename, f_ext = os.path.splitext(filepath)
-    if f'ratio_{str(ratio)}' not in filename:
-        newfilepath = os.path.join(new_trk_folder, filename + f'_ratio_{str(ratio)}'+f_ext)
+    if f_ext == '.trk' and f'ratio_{str(ratio)}' not in filename:
+        newfilename = filename.replace('_all','_ratio_'+str(ratio))
+        newfilepath = os.path.join(new_trk_folder, newfilename +f_ext)
         if not os.path.exists(newfilepath):
             print(f'Downsampling from {os.path.join(trk_folder,filepath)} to {newfilepath} with ratio {str(ratio)}')
             reducetractnumber(os.path.join(trk_folder,filepath), newfilepath, getdata=False, ratio=100, return_affine= False, verbose=False)
--- fix_trk_files.py
+++ fix_trk_files.py
@@ -4,6 +4,7 @@ import shutil
 from tract_handler import trk_fixer
 from file_tools import mkcdir
 from tract_handler import reducetractnumber, ratio_to_str, get_ratio
+import random
 
 TRK_folder = '/Users/alex/jacques/AMD_TRK_testing/TRK_MDT'
 TRK_output = '/Users/alex/jacques/AMD_TRK_testing/TRK_MDT_fixed'
@@ -14,12 +15,13 @@ TRK_output = '/Volumes/Data/Badea/Lab/human/AMD/TRK_MDT_fixed'
 TRK_folder = '/mnt/munin6/Badea/Lab/human/AD_Decode/Analysis/TRK_MPCA_MDT/'
 TRK_output = '/mnt/munin6/Badea/Lab/human/AD_Decode/Analysis/TRK_MPCA_MDT_fixed/'
 
-TRK_folder = '/mnt/paros_MRI/jacques/AD_Decode/Analysis/TRK_MPCA_MDT/'
-TRK_output = '/mnt/paros_MRI/jacques/AD_Decode/Analysis/TRK_MPCA_MDT_fixed/'
+TRK_folder = '/Volumes/Data/Badea/Lab/human/AD_Decode/Analysis/TRK_MPCA_MDT_fixed/'
+TRK_output = '/Volumes/Data/Badea/Lab/human/AD_Decode/Analysis/TRK_MPCA_MDT_fixed_refixed/'
 
 filelist = os.listdir(TRK_folder)
 filelist = sorted(filelist)
 #filelist.reverse()
+random.shuffle(filelist)
 
 mkcdir(TRK_output)
 for trk_name in filelist:
